f1424412f27426e72480fa121889698f2ba7c206
Felipe Slaughter-Quintero <fslauq@gmail.com>
2021-08-11 21:26:08 -0400

    monolith push

--- index.js
+++ index.js
@@ -3,11 +3,20 @@ const express = require('express');
 const cors = require('cors');;
 const app = express();
 const port = process.env.PORT || 9000;
+const path = require('path');
+
+// const secret = process.env.SECRET || 'secret';
 
 app.use(cors());
+app.use(express.json());
+app.use(express.static(path.join(__dirname, 'client/build'))); //handles homepage
 
 app.use("/api/*",(_,res)=>{
   res.json({data:"The API is working"});
+});
+
+app.use("*", (_,res)=>{ //handles all other routes or incorrect endpoints
+  res.sendFile(path.join(__dirname, 'client/build/index.html'));
 });
 
 app.listen(port, () => {
